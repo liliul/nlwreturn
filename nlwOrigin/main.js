cd0865f16550fd4943cb48f52232a2342197972d
liliul <liliuestudo@gmail.com>
2022-05-06 03:56:34 -0300

    enviar para github

---
+++ nlwOrigin/main.js
@@ -0,0 +1,45 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+function onScroll() {
+	showNavOnScroll()
+	showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll() {
+	if (scrollY > 0) {
+		navigation.classList.add('scroll')
+	} else {
+		navigation.classList.remove('scroll')
+	}
+}
+function showBackToTopButtonOnScroll() {
+	if (scrollY > 200) {
+		backToTopButton.classList.add('show')
+	} else {
+		backToTopButton.classList.remove('show')
+	}
+}
+
+function openMenu() {
+	document.body.classList.add('menu-expanded')
+}
+function closeMenu() {
+	document.body.classList.remove('menu-expanded')
+}
+
+
+
+
+
+ScrollReveal({
+	origin: 'top',
+	distance: '0px',
+	duration: 700,  
+}).reveal(`
+	#home,
+	#home img,
+	#home .numbers,
+	#services,
+	#services header,
+	#services .card`)
